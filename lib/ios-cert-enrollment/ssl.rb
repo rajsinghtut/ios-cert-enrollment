4869cd3c6f009b588fe688f577a2d652ae86cc18
Raj Singh Tut <raj.tut@gmail.com>
2016-04-04 15:38:49 +0100

    pass in cert if available

--- lib/ios-cert-enrollment/ssl.rb
+++ lib/ios-cert-enrollment/ssl.rb
@@ -4,11 +4,13 @@ module IOSCertEnrollment
     class << self    
       def key
         return @@key if @@key
+        return @@key = OpenSSL::PKey::RSA.new(IOSCertEnrollment.ssl_key) if IOSCertEnrollment.ssl_key.present?
         return @@key = OpenSSL::PKey::RSA.new(File.read(IOSCertEnrollment.ssl_key_path))
       end
     
       def certificate
         return @@certificate if @@certificate
+        return @@certificate = OpenSSL::X509::Certificate.new(IOSCertEnrollment.ssl_certificate)) if IOSCertEnrollment.ssl_certificate.present?
         return @@certificate = OpenSSL::X509::Certificate.new(File.read(IOSCertEnrollment.ssl_certificate_path))
       end
     end
